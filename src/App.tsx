b3883b63dfb34cf6f11b21902df35975749ffe74
KMPCH <80095731+NutKamonphop@users.noreply.github.com>
2024-02-22 03:12:21 +0700

    Add file to folder

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css'
+import Calculate from './components/Calculate'
+
+
+function App() {
+    return (<Calculate/>)
+}
+
+export default App
---
+++ src/components/Calculate.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useState} from 'react'
+
+const Calculate = () => {
+
+    const [Weight, setWeight] = useState<number>(0);
+    const [Result, setResult] = useState<number>(0);
+
+      const onChangeWeight = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setWeight(+event.target.value)
+      }  
+
+      const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        setResult(Weight*2.2*30/2)
+      }
+
+      return (
+        <>
+        <div>
+          <form onSubmit={onSubmit}>
+            <h1>ควรดื่มน้ำวันละเท่าไหร่ ?</h1>
+            <h2>{Result} มล.</h2>
+            <input type="number" id="Weight" name="Weight" placeholder='Weight (KG)' onChange={onChangeWeight}/>
+            <br />
+            <button type='submit'>Calculate</button>          
+          </form>
+        </div>
+        </>
+      )
+    }
+
+export default Calculate
